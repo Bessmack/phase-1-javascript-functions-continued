7033c4b8c416267c90baac42db27a5edf316ac9f
Bessmack <jenwied4lif@gmail.com>
2025-06-07 19:22:44 +0300

    complete defining functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+mondayWork("work from home");
+
+
+function wrapAdjective(character = "*"){
+    return function(adjective = "special"){
+        return `You are ${character}${adjective}${character}!`;
+    };
+}
+wrapAdjective()("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
